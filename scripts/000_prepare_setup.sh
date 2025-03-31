6ec251c463b8fe95386e3fc74b6a325eee46d333
vvpreo <vvpreo@gmail.com>
2025-04-01 03:47:03 +0700

    Tuning dev container

--- scripts/000_prepare_setup.sh
+++ scripts/000_prepare_setup.sh
@@ -3,13 +3,18 @@
 # Activate virtual environment if it exists
 if [ -d ".venv" ]; then
     echo "Activating virtual environment..."
+    # shellcheck disable=SC2039
     source .venv/bin/activate
 else
     echo "Creating python virtual environment"
     python -m venv .venv
     echo "Activating virtual environment..."
+    # shellcheck disable=SC2039
     source .venv/bin/activate
 fi
 
+# Install dependencies
 pip install uv
 uv pip install -r requirements.txt
+
+echo "DO NOT FORGET to 'source .env' if needed."
