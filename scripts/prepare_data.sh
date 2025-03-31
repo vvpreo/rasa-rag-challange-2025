5e2f2042626a77a46d8cc12da519a168202dbeba
vvpreo <vvpreo@gmail.com>
2025-04-01 02:00:51 +0700

    added conversational prepared data

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 ooo="$@"
-
+_type='conversational'
 echo "STARTING"
-rasa llm finetune prepare-data --num-rephrases 1 --train-frac 0.8 --output-format instruction --out "$ooo/output" $ooo
+rasa llm finetune prepare-data --num-rephrases 1 --train-frac 0.8 --output-format $_type --out "$ooo/output_$_type" $ooo
 echo "DONE"
